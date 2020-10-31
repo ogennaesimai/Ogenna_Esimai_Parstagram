5cc27c4237be3e2010be30f6aea490934141d92a
Ogenna Esimai <ogenna.esimai@mavs.uta.edu>
2020-10-31 02:07:13 -0500

    Added Part 2

---
+++ README.md
@@ -0,0 +1,62 @@
+# Project 3 - Ogenna_Esimai_Parstagram
+
+Ogenna_Esimai_Parstagram is a photo sharing app similar to Instagram but using Parse as its backend.
+
+Time spent: >5 hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+- [ ] (partial) User can sign up to create a new account using Parse authentication.
+- [x] (for a) User can a) log in and b) log out of his or her account.
+- [x] The current signed in user is persisted across app restarts.
+- [x] (for a), c)) User can a) take a photo, b) add a caption, and c) post it to "Instagram".
+
+The following **optional** features are implemented:
+
+- [ ] User sees app icon in home screen and styled bottom navigation view
+- [ ] Style the feed to look like the real Instagram feed.
+- [ ] After the user submits a new post, show an indeterminate progress bar while the post is being uploaded to Parse.
+
+The following **additional** features are implemented:
+
+- [x] Sign Up button created
+- [x] Toast shows on issue with Sign Up
+- [x] Toast shows on success with Log In
+- [x] Toast shows on issue with Log In from absent username and password
+- [x] Toast shows on issue with Log In from absent password
+- [x] Toast shows if picture was not taken
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://github.com/ogennaesimai/Ogenna_Esimai_Parstagram/blob/master/Ogenna_Esimai_Parstagram_Walkthrough.gif' title='Ogenna_Esimai_Parstagram_Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## Open-source libraries used
+
+- [Android Async HTTP](https://github.com/codepath/CPAsyncHttpClient) - Simple asynchronous HTTP requests with JSON parsing
+- [Glide](https://github.com/bumptech/glide) - Image loading and caching library for Android
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/example/ogenna_esimai_parstagram/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.ogenna_esimai_parstagram;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.ogenna_esimai_parstagram.fragments.ComposeFragment;
+import com.example.ogenna_esimai_parstagram.fragments.PostsFragment;
+import com.example.ogenna_esimai_parstagram.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    private BottomNavigationView bottomNavigationView;
+    private android.view.Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home clicked", Toast.LENGTH_LONG).show();
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "Compose clicked", Toast.LENGTH_LONG).show();
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile clicked", Toast.LENGTH_LONG).show();
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+}
