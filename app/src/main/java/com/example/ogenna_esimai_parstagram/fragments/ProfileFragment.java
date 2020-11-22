99f020e4cfab1ff782e6d8f87aca8f3079de5b39
Ogenna Esimai <ogenna.esimai@mavs.uta.edu>
2020-11-22 00:29:20 -0600

    Updated app look and feel

--- app/src/main/java/com/example/ogenna_esimai_parstagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/ogenna_esimai_parstagram/fragments/ProfileFragment.java
@@ -2,23 +2,22 @@ package com.example.ogenna_esimai_parstagram.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.ogenna_esimai_parstagram.LoginActivity;
+import com.example.ogenna_esimai_parstagram.MainActivity;
 import com.example.ogenna_esimai_parstagram.Post;
 import com.example.ogenna_esimai_parstagram.PostsAdapter;
 import com.example.ogenna_esimai_parstagram.R;
@@ -30,51 +29,47 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ProfileFragment extends Fragment {
 
+    public static final String TAG = "ProfileFragment";
     private RecyclerView rvPosts;
-    public SwipeRefreshLayout swipeContainer;
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
+    public SwipeRefreshLayout swipeContainer;
     private Button btnLogOut;
-
-    public static final String TAG = "ProfileFragment";
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    // This event is triggered soon after onCreateView().
-    // onViewCreated() is only called if the view returned from onCreateView() is non-null.
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
-        //return inflater.inflate(R.layout.tabbar_layout, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         rvPosts = view.findViewById(R.id.rvPosts);
-        allPosts = new ArrayList<>();
-        adapter = new PostsAdapter(getContext(), allPosts);
 
         btnLogOut = view.findViewById(R.id.btnLogOutProfileInNestedLinearLayout);
-
         btnLogOut.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+                Log.i(TAG, ParseUser.getCurrentUser().toString());
                 ParseUser.logOut();
                 ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+
                 Intent startLoginActivityIntent = new Intent(getContext(), LoginActivity.class);
                 startActivity(startLoginActivityIntent);
             }
-        });
 
+        });
         // Steps to use the recycler view:
         // 1. create the adapter
         allPosts = new ArrayList<>();
@@ -83,33 +78,36 @@ public class ProfileFragment extends Fragment {
         // Lookup the swipe container view
         swipeContainer = view.findViewById(R.id.swipeContainer);
         // Setup refresh listener which triggers new data loading
-            swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 // Your code to refresh the list here.
                 // Make sure you call swipeContainer.setRefreshing(false)
                 // once the network request has completed successfully.
                 Log.i(TAG, "fetching new data!");
-                swipeContainer.setRefreshing(false);
+
                 queryPosts();
             }
         });
         // Configure the refreshing colors
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-        android.R.color.holo_green_light,
-        android.R.color.holo_purple,
-        android.R.color.holo_red_light);
+                android.R.color.holo_green_light,
+                android.R.color.holo_purple,
+                android.R.color.holo_red_light);
 
         // Steps to use the recycler view:
         // 0. create layout for one row in the list
+
         // 1. create the adapter
         // 2. create the data source
+
         // 3. set the adapter on the recycleron view
         rvPosts.setAdapter(adapter);
         // 4. set the layout manager on the recycler view
         // by default the code below will provide a vertical LinLayoutMgr to the recycler view
         // which is what we want
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
         queryPosts();
     }
 
@@ -134,7 +132,12 @@ public class ProfileFragment extends Fragment {
 
                 allPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+
             }
         });
+
     }
 }
+
+
